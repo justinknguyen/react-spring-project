1141afe40b4e9f24ea19049c4771a9f06cc2da54
justinknguyen <justinknguyen11@gmail.com>
2022-11-23 11:44:12 -0700

    login page

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import "./App.css";
 import Routes from "./Routes";
+import "./App.css";
 
 function App() {
   return (
     <div className="App container py-3">
       <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
         <Navbar.Brand className="font-weight-bold text-muted">
-          Scratch
+          Student Registration
         </Navbar.Brand>
         <Navbar.Toggle />
       </Navbar>
--- react-frontend/src/Routes.js
+++ react-frontend/src/Routes.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./containers/Login";
+import NotFound from "./containers/NotFound";
 
 export default function Links() {
   return (
     <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/react-spring-project/" element={<Login />} />
+        {
+          /* Finally, catch all unmatched routes */
+        }
+        <Route path="*" element={<NotFound />} />;
     </Routes>
   );
 }
