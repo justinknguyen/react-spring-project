b3f965c034ee96e5148606c0c63d32101a55bfba
justinknguyen <justinknguyen11@gmail.com>
2022-11-22 20:47:38 -0700

    test

--- .history/src/App_20221122204625.js
+++ .history/src/App_20221122204625.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi boss
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        </a>
       </header>
     </div>
   );
