b86839ec53c7a99b47f9c09fb9394c5f82be2a90
Douglas Yau <56883284+digug@users.noreply.github.com>
2022-11-26 01:58:26 -0700

    Implement updateStudentPassword fun as a student service

--- spring-backend/src/main/java/course/ensf607/assignment6/student/StudentService.java
+++ spring-backend/src/main/java/course/ensf607/assignment6/student/StudentService.java
@@ -2,6 +2,7 @@ package course.ensf607.assignment6.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -48,5 +49,14 @@ public class StudentService {
     public Optional<Student> searchStudentByUcid(String ucid) {
         Optional<Student> studentByUcid = studentRepository.findStudentByUcid(ucid);
         return studentByUcid;
+    }
+    @Transactional
+    public void updateStudentPassword(Long studentId, String oldPassword, String newPassword) {
+        Student studentById = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("User not found"));
+        if (oldPassword.length() > 0 && !oldPassword.equals(studentById.getPassword())){
+            throw new IllegalArgumentException("Wrong password");
+        }
+        studentById.setPassword(newPassword);
     }
 }
