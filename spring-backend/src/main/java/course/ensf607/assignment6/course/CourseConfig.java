1779a696f5692c08d78a76c143ea73a253ee7280
justinknguyen <justinknguyen11@gmail.com>
2022-11-26 15:17:36 -0700

    aws database config

--- spring-backend/src/main/java/course/ensf607/assignment6/course/CourseConfig.java
+++ spring-backend/src/main/java/course/ensf607/assignment6/course/CourseConfig.java
@@ -1,60 +1,60 @@
-//package course.ensf607.assignment6.course;
-//
-//import course.ensf607.assignment6.student.Student;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.time.LocalDate;
-//import java.util.HashSet;
-//import java.util.List;
-//
-//@Configuration
-//public class CourseConfig {
-//    @Bean
-//    CommandLineRunner createCourse(CourseRepository courseRepository){
-//        return args -> {
-//            Course one = new Course(
-//                    "ENSF607",
-//                    LocalDate.now(),
-//                    LocalDate.now().plusMonths(4),
-//                    60,
-//                    false,
-//                    new HashSet<Student>()
-//            );
-//
-//            Course two = new Course(
-//                    "ENSF608",
-//                    LocalDate.now(),
-//                    LocalDate.now().plusMonths(4),
-//                    60,
-//                    false,
-//                    new HashSet<Student>()
-//            );
-//
-//            Course three = new Course(
-//                    "ENSF611",
-//                    LocalDate.now(),
-//                    LocalDate.now().plusMonths(4),
-//                    60,
-//                    false,
-//                    new HashSet<Student>()
-//            );
-//
-//            Course four = new Course(
-//                    "ENSF614",
-//                    LocalDate.now(),
-//                    LocalDate.now().plusMonths(4),
-//                    60,
-//                    false,
-//                    new HashSet<Student>()
-//            );
-//
-//            courseRepository.saveAll(
-//                    List.of(
-//                            one, two, three, four
-//                    )
-//            );
-//        };
-//    }
-//}
+package course.ensf607.assignment6.course;
+
+import course.ensf607.assignment6.student.Student;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+
+@Configuration
+public class CourseConfig {
+    @Bean
+    CommandLineRunner createCourse(CourseRepository courseRepository){
+        return args -> {
+            Course one = new Course(
+                    "ENSF607",
+                    LocalDate.now(),
+                    LocalDate.now().plusMonths(4),
+                    60,
+                    false,
+                    new HashSet<Student>()
+            );
+
+            Course two = new Course(
+                    "ENSF608",
+                    LocalDate.now(),
+                    LocalDate.now().plusMonths(4),
+                    60,
+                    false,
+                    new HashSet<Student>()
+            );
+
+            Course three = new Course(
+                    "ENSF611",
+                    LocalDate.now(),
+                    LocalDate.now().plusMonths(4),
+                    60,
+                    false,
+                    new HashSet<Student>()
+            );
+
+            Course four = new Course(
+                    "ENSF614",
+                    LocalDate.now(),
+                    LocalDate.now().plusMonths(4),
+                    60,
+                    false,
+                    new HashSet<Student>()
+            );
+
+            courseRepository.saveAll(
+                    List.of(
+                            one, two, three, four
+                    )
+            );
+        };
+    }
+}
--- spring-backend/src/main/java/course/ensf607/assignment6/student/StudentConfig.java
+++ spring-backend/src/main/java/course/ensf607/assignment6/student/StudentConfig.java
@@ -1,35 +1,35 @@
-//package course.ensf607.assignment6.student;
-//
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.List;
-//
-//@Configuration
-//public class StudentConfig {
-//
-//    @Bean
-//    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
-//        return args -> {
-//            Student doug = new Student(
-//                    "digug",
-//                    "12345",
-//                    "30030377",
-//                    null
-//
-//            );
-//            Student justin = new Student(
-//                    "justinn",
-//                    "123456",
-//                    "1111111",
-//                    null
-//
-//            );
-//
-//            studentRepository.saveAll(
-//                    List.of(doug, justin)
-//            );
-//        };
-//    };
-//}
+package course.ensf607.assignment6.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student doug = new Student(
+                    "digug",
+                    "12345",
+                    "30030377",
+                    null
+
+            );
+            Student justin = new Student(
+                    "justinn",
+                    "123456",
+                    "1111111",
+                    null
+
+            );
+
+            studentRepository.saveAll(
+                    List.of(doug, justin)
+            );
+        };
+    };
+}
