10245877115ea6632ba82a7277bc57d83ef7b7a3
Douglas Yau <56883284+digug@users.noreply.github.com>
2022-11-25 19:24:11 -0700

    Implement deleteStudent by id method

--- spring-backend/src/main/java/course/ensf607/assignment6/student/StudentController.java
+++ spring-backend/src/main/java/course/ensf607/assignment6/student/StudentController.java
@@ -26,4 +26,10 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
 }
