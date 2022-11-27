969213ea10c7627c2e340a572557365d2cf34ba3
Douglas Yau <56883284+digug@users.noreply.github.com>
2022-11-27 15:19:04 -0700

    Add web CORS config to allow wildcard ports

--- spring-backend/src/main/java/course/ensf607/assignment6/Assignment6Application.java
+++ spring-backend/src/main/java/course/ensf607/assignment6/Assignment6Application.java
@@ -2,6 +2,9 @@ package course.ensf607.assignment6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Assignment6Application {
@@ -10,4 +13,13 @@ public class Assignment6Application {
         SpringApplication.run(Assignment6Application.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry reg){
+                reg.addMapping("/**").allowedHeaders("*").allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
 }
